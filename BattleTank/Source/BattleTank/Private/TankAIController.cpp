aae84f425e459a55dd65a20abc7c0facbcd4f822
Ján Paraska <jan.paraska@student.upjs.sk>
2018-12-25 16:20:50 +0100

    BT 105 Fixing errors #2

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -19,7 +19,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)) return;
+	if (!(PlayerTank && ControlledTank)) return;
 
 	// Move towards player
 	MoveToActor(PlayerTank, AcceptanceRadius);
