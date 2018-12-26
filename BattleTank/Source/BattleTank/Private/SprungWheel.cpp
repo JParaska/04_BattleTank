bf3259eb7baf6fd236d1cd9f72a04fffd0a61c15
Ján Paraska <jan.paraska@student.upjs.sk>
2018-12-26 20:29:44 +0100

    BT108  Spawning sprung wheel on tank

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,43 @@
+// Jan Paraska
+
+#include "SprungWheel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheel Constraint"));
+	SetRootComponent(MassWheelConstraint);
+
+	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	Mass->SetupAttachment(MassWheelConstraint);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(MassWheelConstraint);
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (GetAttachParentActor()) {
+		UE_LOG(LogTemp, Warning, TEXT("Parent of %s is %s"), *GetName(), *GetAttachParentActor()->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("%s has no parent"), *GetName());
+	}
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ BattleTank/Source/BattleTank/Public/SpawnPoint.h
@@ -0,0 +1,33 @@
+// Jan Paraska
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "SpawnPoint.generated.h"
+
+class ASprungWheel;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API USpawnPoint : public USceneComponent
+{
+	GENERATED_BODY()
+
+/** Properties */
+private: 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<ASprungWheel> Wheel;
+
+/** Methods */
+public:	
+	// Sets default values for this component's properties
+	USpawnPoint();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;		
+	
+};
